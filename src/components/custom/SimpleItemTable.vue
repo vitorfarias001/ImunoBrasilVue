cfb41dd62b946936bf8672c18405cf92ab4b103a
Renan Joppert <renan@guaidor.digital>
2022-12-16 11:35:53 -0300

    Páginas estáticas

--- src/components/custom/SimpleItemTable.vue
+++ src/components/custom/SimpleItemTable.vue
@@ -1,133 +1,188 @@
 <template>
-    <el-table v-loading="loading" class="custom-table" 
-        :data="rows" style="width: 100%">
-        <el-table-column v-for="(column, index) in columns" :key="index" :label="(column as ITableColumn).display">
-            <template #default="item">
-                <span  v-if="!item.row.isEditing">{{item.row[(column as ITableColumn).key]}}</span>
-                <v-text-field density="compact"  v-if="item.row.isEditing" v-model="item.row[(column as ITableColumn).key]" variant="outlined"></v-text-field>
-            </template>
-        </el-table-column>
-        <el-table-column v-if="UseEdit" width="150" fixed="right" label="">
-          <template #default="item">
-            <el-tooltip
-            content="Editar"
-            placement="top"
-            effect="light"
-            >
-                <font-awesome-icon v-if="!item.row.isEditing" @click="(item.row.isEditing = true)" class="simpletable-edit-btn" style="cursor: pointer; font-size: 22px; width: 22px; height: 22px; color: #a3a6b4 !important;"  :icon="['fas',  'pen-to-square']" />
-            </el-tooltip>
-            <el-tooltip
-            content="Salvar"
-            placement="top"
-            effect="light"
-            >
-            <font-awesome-icon @click="deletar(item.row as any)" v-if="item.row.isEditing" class="simpletable-edit-btn text-secondary  mb-4" style="cursor: pointer; font-size: 22px; width: 22px; height: 22px;"  :icon="['fas',  'save']" />
-            </el-tooltip>
-            <el-tooltip
-            content="Deletar"
-            placement="top"
-            effect="light"
-            >
-              <font-awesome-icon @click="salvar(item.row as any)" v-if="item.row.isEditing" class="simpletable-edit-btn ml-4 mb-4" style="cursor: pointer; font-size: 22px; width: 22px; height: 22px;color: red !important;"  :icon="['fas',  'trash']" />
-            </el-tooltip>
-          </template>
-        </el-table-column>
-      </el-table>
-      <v-row class="px-3 mt-2">
-        <v-col cols="6">
-            <span class="text-primary">Exibindo {{(pagination as IPagedResponseBase).pageSize <= (pagination as IPagedResponseBase).total ? (pagination as IPagedResponseBase).pageSize : (pagination as IPagedResponseBase).total}} de {{(pagination as IPagedResponseBase).total}}</span>
-        </v-col>
-        <v-col class="d-flex" cols="6">
-            <el-pagination class="ml-auto" v-model="pagination.page" :page-size="(pagination as IPagination).pageSize" 
-      :pager-count="(pagination as IPagination).totalPages" layout="prev, pager, next" :total="(pagination as IPagination).totalRows" />
-        </v-col>
-      </v-row>
-      
-  </template>
-  
+  <el-table
+    v-loading="loading"
+    class="custom-table"
+    :data="rows"
+    style="width: 100%"
+  >
+    <el-table-column
+      v-for="(column, index) in columns"
+      :key="index"
+      :label="(column as ITableColumn).display"
+    >
+      <template #default="item">
+        <span v-if="!item.row.isEditing">{{
+          item.row[(column as ITableColumn).key]
+        }}</span>
+        <v-text-field
+          density="compact"
+          v-if="item.row.isEditing"
+          v-model="item.row[(column as ITableColumn).key]"
+          variant="outlined"
+        ></v-text-field>
+      </template>
+    </el-table-column>
+    <el-table-column v-if="UseEdit" width="150" fixed="right" label="">
+      <template #default="item">
+        <el-tooltip content="Editar" placement="top" effect="light">
+          <font-awesome-icon
+            v-if="!item.row.isEditing"
+            @click="item.row.isEditing = true"
+            class="simpletable-edit-btn"
+            style="
+              cursor: pointer;
+              font-size: 22px;
+              width: 22px;
+              height: 22px;
+              color: #a3a6b4 !important;
+            "
+            :icon="['fas', 'pen-to-square']"
+          />
+        </el-tooltip>
+        <el-tooltip content="Salvar" placement="top" effect="light">
+          <font-awesome-icon
+            @click="deletar(item.row as any)"
+            v-if="item.row.isEditing"
+            class="simpletable-edit-btn text-secondary mb-4"
+            style="cursor: pointer; font-size: 22px; width: 22px; height: 22px"
+            :icon="['fas', 'save']"
+          />
+        </el-tooltip>
+        <el-tooltip content="Deletar" placement="top" effect="light">
+          <font-awesome-icon
+            @click="salvar(item.row as any)"
+            v-if="item.row.isEditing"
+            class="simpletable-edit-btn ml-4 mb-4"
+            style="
+              cursor: pointer;
+              font-size: 22px;
+              width: 22px;
+              height: 22px;
+              color: red !important;
+            "
+            :icon="['fas', 'trash']"
+          />
+        </el-tooltip>
+      </template>
+    </el-table-column>
+  </el-table>
+  <v-row class="px-3 mt-2">
+    <v-col cols="6">
+      <span class="text-primary"
+        >Exibindo
+        {{
+          (pagination as IPagedResponseBase).pageSize <=
+          (pagination as IPagedResponseBase).total
+            ? (pagination as IPagedResponseBase).pageSize
+            : (pagination as IPagedResponseBase).total
+        }}
+        de {{ (pagination as IPagedResponseBase).total }}</span
+      >
+    </v-col>
+    <v-col class="d-flex" cols="6">
+      <el-pagination
+        class="ml-auto"
+        v-model="pagination.page"
+        :page-size="(pagination as IPagination).pageSize"
+        :pager-count="(pagination as IPagination).totalPages"
+        layout="prev, pager, next"
+        :total="(pagination as IPagination).totalRows"
+      />
+    </v-col>
+  </v-row>
+</template>
+
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { IPagedResponseBase } from '../../api/types/base'
+import { defineComponent, ref } from "vue";
+import { IPagedResponseBase } from "../../api/types/base";
 
 export interface ITableColumn {
-    key: string,
-    display: string
+  key: string;
+  display: string;
 }
 
 export interface IPagination {
-    page: number,
-    pageSize: number,
-    totalPages: number,
-    totalRows: number
+  page: number;
+  pageSize: number;
+  totalPages: number;
+  totalRows: number;
 }
-
 
 export default defineComponent({
-    props:{
-        UseEdit: {
-            type: Boolean,
-            required: false,
-            default: false
-        },
-        loading: {
-            type: Boolean,
-            required: false,
-            default: false
-        },
-        columns: {
-            type: Array,
-            required: true,
-            default: [] as ITableColumn[]
-        },
-        rows: {
-            type: Array,
-            required: true
-        },
-        pagination: {
-            type: Object,
-            required: true,
-            default: {} as IPagedResponseBase
-        }
+  props: {
+    UseEdit: {
+      type: Boolean,
+      required: false,
+      default: false,
     },
-    setup() {
-        return {
-        }
+    debug: {
+      type: Boolean,
+      required: false,
+      default: false,
     },
-    methods:{
-        deletar(item: any):void{
-            this.$emit('deletar', item.id);
-            item.isEditing = false
-        },
-        salvar(item: any):void{
-            this.$emit('salvar', item);
-            item.isEditing = false
-        }
-    }
-})
+    loading: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    columns: {
+      type: Array,
+      required: true,
+      default: [] as ITableColumn[],
+    },
+    rows: {
+      type: Array,
+      required: true,
+    },
+    pagination: {
+      type: Object,
+      required: true,
+      default: {} as IPagedResponseBase,
+    },
+  },
+  setup(props) {
+    return {};
+  },
+  methods: {
+    deletar(item: any): void {
+      this.$emit("deletar", item.id);
+      item.isEditing = false;
+    },
+    salvar(item: any): void {
+      this.$emit("salvar", item);
+      item.isEditing = false;
+    },
+  },
+  watch: {
+    rows(to) {
+      console.log("Watch Rows", to);
+    },
+    pagination(to) {
+      console.log("Pagination Rows", to);
+    },
+    columns(to) {
+      console.log("Columns Rows", to);
+    },
+  },
+});
 </script>
 
-
 <style lang="scss">
-
-th.el-table__cell{
-    height: auto;
-        padding: 20px !important;
-        background-color: #a3a6b430 !important;
+th.el-table__cell {
+  height: auto;
+  padding: 20px !important;
+  background-color: #a3a6b430 !important;
 }
 
-td.el-table__cell{
-    padding: 20px !important;
+td.el-table__cell {
+  padding: 20px !important;
 }
 
-svg.simpletable-edit-btn{
-
+svg.simpletable-edit-btn {
 }
 
-
-svg.simpletable-edit-btn:hover
-svg.simpletable-edit-btn:focus {
-    color: red;
-    transform: translateX(10px) scale(1.2);  
+svg.simpletable-edit-btn:hover svg.simpletable-edit-btn:focus {
+  color: red;
+  transform: translateX(10px) scale(1.2);
 }
-
 </style>
---
+++ src/pages/tipos-tratamento/list.vue
@@ -0,0 +1,222 @@
+<template>
+  <v-container>
+    <v-row no-gutters>
+      <div class="header-page">
+        <v-col>
+          <header>
+            <div class="text-h4 text-primary">Tipos de Tratamentos</div>
+          </header>
+        </v-col>
+        <v-col cols="2">
+          <v-btn variant="flat" color="secondary" class="btn-primary">
+            Adicionar
+          </v-btn>
+        </v-col>
+        <v-col cols="2">
+          <div
+            class="d-flex justify-space-around align-center flex-column flex-sm-row fill-height"
+          >
+            <v-text-field
+              density="compact"
+              variant="solo"
+              label="Pesquisar"
+              append-inner-icon="mdi-magnify"
+              single-line
+              hide-details
+            ></v-text-field>
+          </div>
+        </v-col>
+      </div>
+    </v-row>
+
+    <v-row no-gutters>
+      <v-col>
+        <!-- <simple-item-table
+        :UseEdit="true" :loading="LoadingTiposTratamento" :pagination="tipoTratamentoPages" 
+        :columns="columns" :rows="cids" ></simple-item-table> -->
+        <simple-item-table
+          :use-edit="true"
+          :rows="rows"
+          :columns="columns"
+          :loading="loading"
+        ></simple-item-table>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import ApiServices from "../../api";
+
+import { ICidDto, ICidFiltroRequest } from "../../api/types/cid";
+import SimpleItemTable, {
+  ITableColumn,
+} from "../../components/custom/SimpleItemTable.vue";
+import {
+  IPagedResponse,
+  IPagedResponseBase,
+  ISearchDataParams,
+} from "../../api/types/base";
+import { ITipoTratamentoDto } from "../../api/types/tipoTratamento";
+
+export default defineComponent({
+  components: { SimpleItemTable },
+  // setup() {
+  //   const LoadingTiposTratamento = ref(false);
+  //   const cids = ref([] as ICidDto[])
+  //   const tipoTratamentoPages = ref({} as IPagedResponseBase)
+  //   return{
+  //     LoadingTiposTratamento,
+  //     cids,
+  //     tipoTratamentoPages
+  //   }
+  // },
+  mounted() {
+    this.fetchTiposTratamento();
+  },
+  data() {
+    return {
+      page: 1,
+      columns: [
+        {
+          key: "tipo",
+          display: "Tipo",
+        },
+        {
+          key: "precisaAutorizacao",
+          display: "Precisa de Autorização?",
+        },
+        {
+          key: "time",
+          display: "Time",
+        },
+        {
+          key: "especialidade",
+          display: "Especialidade",
+        },
+      ],
+      loading: false,
+      rows: [],
+    };
+  },
+  methods: {
+    async fetchTiposTratamento() {
+      this.loading = true;
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `bearer ${localStorage.getItem("IMB:Token")}`,
+        },
+        body: JSON.stringify({}),
+      };
+
+      const baseUrl = import.meta.env.VITE_API_URL;
+
+      fetch(`${baseUrl}/TipoTratamento/Busca`, requestOptions)
+        .then(async (response) => {
+          const data = await response.json();
+          this.rows = data.results;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    // async buscarCids(){
+    //   this.LoadingTiposTratamento = true;
+    //   var cidResponse = await ApiServices.CidApi.BuscaCid({paginacao: { page: 1, pageSize: 50 }, filtros: {} as ICidFiltroRequest } as ISearchDataParams<ICidFiltroRequest>)
+
+    //     if(cidResponse.sucesso){
+    //     this.cids = cidResponse.corpo.results
+    //     this.tipoTratamentoPages = {currentPage: cidResponse.corpo.currentPage, pageCount: cidResponse.corpo.pageCount, pageSize: cidResponse.corpo.pageSize, total: cidResponse.corpo.total }
+    //     }
+    //     else
+    //     {
+    //       this.$moshaToast(cidResponse.mensagem, { type: 'danger' });
+    //     }
+    //     this.LoadingTiposTratamento = false;
+    // },
+    async updateCid(item: ICidDto) {
+      // var updateResponse = await ApiServices.CidApi.Update(item.id, item);
+    },
+    async removeCid(id: number) {},
+  },
+});
+</script>
+
+<style lang="scss">
+@media (min-width: 1280px) {
+  .v-container {
+    max-width: 100% !important;
+  }
+}
+.header-page {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  .title-page {
+    color: #005356;
+    font-size: 3rem;
+  }
+  .btn-primary {
+    background-color: #80c340 !important;
+    color: #ffffff !important;
+    min-width: 64px;
+    border-radius: 4px;
+    width: 180px;
+    font-weight: 500;
+    letter-spacing: 0.02857em;
+    text-transform: uppercase;
+    line-height: 3.5;
+    min-height: 45px;
+    float: right;
+    &:hover {
+      background: #80c34090 !important;
+    }
+  }
+
+  .v-field {
+    border-radius: 3rem;
+    box-shadow: none;
+    font-size: 1rem;
+    min-height: 45px;
+    line-height: 1.75;
+  }
+}
+
+.dialog-page {
+  .v-card {
+    padding: 1.4rem;
+  }
+  .v-card-title {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .v-card-text {
+    padding: 16px 10px 10px !important;
+  }
+  .v-card-actions {
+    display: flex;
+    justify-content: flex-end;
+    .v-btn {
+      background-color: #80c340 !important;
+      color: #ffffff !important;
+      min-width: 64px;
+      border-radius: 4px;
+      width: 180px;
+      font-weight: 500;
+      letter-spacing: 0.02857em;
+      text-transform: uppercase;
+      line-height: 3.5;
+      min-height: 45px;
+      float: right;
+    }
+  }
+}
+
+.v-pagination__list {
+  justify-content: flex-end !important;
+}
+</style>
